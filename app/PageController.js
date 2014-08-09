dbd668a803034f41ca47d9e3f22e51d6e25d0582
Tony Ennis <tony.ennis@twc.com>
2014-08-09 00:16:51 -0400

    Improved promise support

--- app/PageController.js
+++ app/PageController.js
@@ -1,6 +1,6 @@
 angular.module("app", [])
-    .controller("PageController", ['$scope', '$http',
-        function ($scope, $http) {
+    .controller("PageController", ['$scope', '$http', '$q',
+        function ($scope, $http, $q) {
 
             $scope.setSeq = 1;
             $scope.setDoubleIt = 10;
@@ -24,8 +24,12 @@ angular.module("app", [])
                 }
             };
 
+            $scope.waiting = function waiting() {
+                return $scope.status === "spinning up" || $scope.status === "all in-flight";
+            }
+
             var fetch = function fetch(url, value, f) {
-                $http({
+                return $http({
                     method: "GET",
                     url: url,
                     params: {
@@ -41,20 +45,25 @@ angular.module("app", [])
 
             var independent = function independent() {
                 clear();
+                var promises = [];
                 $scope.status = "spinning up";
-                fetch("http://localhost:3000", $scope.setDoubleIt, function (v) {
+                promises.push(fetch("http://localhost:3000", $scope.setDoubleIt, function (v) {
                     $scope.doubleIt = v;
-                });
-                fetch("http://localhost:3001", 7, function (v) {
+                }));
+                promises.push(fetch("http://localhost:3001", 7, function (v) {
                     $scope.content = v;
-                });
-                fetch("http://localhost:3002", 9, function (v) {
+                }));
+                promises.push(fetch("http://localhost:3002", 9, function (v) {
                     $scope.timestamp = v;
-                });
-                fetch("http://localhost:3003", $scope.setSeq, function (v) {
+                }));
+                promises.push(fetch("http://localhost:3003", $scope.setSeq, function (v) {
                     $scope.seq = v;
-                });
+                }));
                 $scope.status = "all in-flight";
+
+                $q.all(promises).then(function () {
+                    $scope.status = "Completed"
+                });
             };
 
             var serial = function serial() {
